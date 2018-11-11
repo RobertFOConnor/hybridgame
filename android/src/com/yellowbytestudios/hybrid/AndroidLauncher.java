a365d1414b9ebfd4b3c9395aa6c6e414e8f8a440
robert.oconnor <robert.oconnor@mediamonks.com>
2018-11-11 19:18:52 +0100

    android device

--- android/src/com/yellowbytestudios/hybrid/AndroidLauncher.java
+++ android/src/com/yellowbytestudios/hybrid/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.yellowbytestudios.hybrid.MainGame;
+import com.yellowbytestudios.hybrid.utils.DeviceTypes;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MainGame(), config);
+		initialize(new MainGame(DeviceTypes.ANDROID), config);
 	}
 }
